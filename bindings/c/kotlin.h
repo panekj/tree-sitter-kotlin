44dea263523f30f7d4ac979f90122873fe2f8637
Jakub Panek <me@panekj.dev>
2024-06-07 23:29:31 +0200

    fix: tree-sitter generate

---
+++ bindings/c/kotlin.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_KOTLIN_H_
+#define TREE_SITTER_KOTLIN_H_
+
+#include <tree_sitter/parser.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_kotlin();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_KOTLIN_H_
